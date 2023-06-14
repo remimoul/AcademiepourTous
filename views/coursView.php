f8d9d931036ff9edd5cf0f8b91a467800fbb07bf
remimoul <neptunes92390@outlook.fr>
2023-06-14 11:36:13 +0200

    implementation of a bottom modal for reservations

--- views/coursView.php
+++ views/coursView.php
@@ -1,7 +1,100 @@
 <?php
 require('header.php');
 ?>
+<style>
+    .bodydrag{
+        background: #E3F2FD;
+    }
 
+    .bottom-sheet {
+        position:fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        opacity: 0;
+        pointer-events: none;
+        flex-direction: column;
+        justify-content: end;
+        align-items: center;
+        transition: 0.1s linear;
+    }
+
+    .bottom-sheet.show{
+        opacity: 1;
+        pointer-events: auto;
+    }
+
+    .bottom-sheet .sheet-overlay{
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: -1;
+        width: 100%;
+        height: 100%;
+        opacity: 0.2;
+        background: black;
+    }
+
+    .bottom-sheet .content{
+        background: #fff;
+        height: 50vh;
+        max-height: 100vh;
+        padding: 25px 30px;
+        width: 100%;
+        max-width: 1100px;
+        border-radius:12px 12px 0 0;
+        position: relative;
+        transform: translateY(100%);
+        transition: 0.3s ease;
+    }
+
+    .bottom-sheet.show .content{
+        transform: translateY(0%);
+    }
+
+    .bottom-sheet .header{
+        display: flex;
+        justify-content: center;
+    }
+
+    .bottom-sheet .drag-icon{
+        cursor: grab;
+        user-select: none;
+        padding: 15px;
+        margin-top: -15px;
+    }
+
+    .bottom-sheet .drag-icon span{
+        height: 4px;
+        width: 40px;
+        display: block;
+        background: #C7D0E1;
+        border-radius:50px;
+    }
+
+    .bottom-sheet .bodydrag {
+        overflow-y:auto;
+        height: 100%;
+        padding: 15px 0 40px;
+        scrollbar-width: none;
+    }
+
+    .bottom-sheet .bodydrag::-webkit-scrollbar{
+        width: 0;
+    }
+
+    .bottom-sheet .bodydrag h2{
+        font-size: 1.8rem;
+    }
+    .bottom-sheet .bodydrag p{
+        font-size: 1.05rem;
+        margin-top: 20px;
+    }
+
+
+</style>
 
 <h1 class="text-6xl font-bold text-center my-12">Voici les cours que nous proposons &#128516;</h1>
 
@@ -17,11 +110,27 @@ require('header.php');
         </p>
     </div>
     <div class="px-6 pt-4 pb-2">
-        <button class="bg-red-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+        <button class="show-modal bg-red-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
 
-            <a href="./views/contactView.php">Réserver  </a>
+            Réserver
         </button>
+        <div class="bottom-sheet">
+            <div class="sheet-overlay"></div>
+            <div class="content">
+                <div class="header">
+                    <div class="drag-icon"><span></span></div>
+                </div>
+                <div class="bodydrag">
+                <h2>Bottom Sheet Modal</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ex ligula, egestas ac purus in, mattis maximus tellus. Sed turpis purus, consectetur id pulvinar ac, fermentum vel tellus. Pellentesque in libero mi. Maecenas auctor lobortis purus, eu viverra nibh maximus eu. Sed maximus eget nisi a tempor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum eu sapien bibendum, suscipit eros a, placerat eros. Nunc nec quam eu quam porttitor auctor nec nec lectus. Mauris molestie arcu urna, eu rutrum odio bibendum eget. Suspendisse id libero erat. Mauris mollis, nibh eget tincidunt efficitur, leo lacus laoreet ante, quis rutrum turpis massa non dolor. Vestibulum luctus congue massa, id tempor odio accumsan in. Etiam nisi ex, pharetra nec sollicitudin eget, rhoncus eu massa. Curabitur sagittis euismod lacus, finibus vulputate nisi. Phasellus dapibus nunc id neque commodo posuere nec nec leo.</p>
+                    <button class="bg-red-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
 
+                        <a href="./views/contactView.php">Réserver  </a>
+                    </button>
+
+            </div>
+        </div>
+    </div>
         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 mx-4"> Tél : 06 11 01 60 83</span>
     </div>
 </div>
@@ -121,6 +230,40 @@ require('header.php');
 </div>
 
 </section>
+
+<script>
+const showModalBtn = document.querySelector(".show-modal");
+const bottomSheet = document.querySelector(".bottom-sheet");
+const sheetOverlay = bottomSheet.querySelector(".sheet-overlay");
+const sheetContent = bottomSheet.querySelector(".content");
+const dragIcon = bottomSheet.querySelector(".drag-icon");
+
+// let isDragging = false;
+
+const showBottomSheet = () => {
+bottomSheet.classList.add("show");
+}
+const hideBottomSheet = () =>{
+    bottomSheet.classList.remove("show");
+}
+
+// const dragStart = () => {
+//     isDragging = true;
+// }
+
+const dragging = (e) =>{
+    // if(!isDragging) return;
+    sheetContent.style.height = `${e.pageY}vh`;
+    console.log(e.pageY);
+}
+
+// dragIcon.addEventListener("mousedown",dragStart);
+// dragIcon.addEventListener("mousedown",dragStart);
+dragIcon.addEventListener("mousemove",dragging);
+showModalBtn.addEventListener("click", showBottomSheet);
+sheetOverlay.addEventListener("click",hideBottomSheet);
+
+</script>
 
 <?php
 
