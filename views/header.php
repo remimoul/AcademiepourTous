33587ffffc64d06bc1aa9a43c6881eaa775f2887
remimoul <neptunes92390@outlook.fr>
2023-06-15 13:20:22 +0200

    maj responsive mobile

--- views/header.php
+++ views/header.php
@@ -5,6 +5,7 @@ ob_start();
 <!DOCTYPE html>
 <html>
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <base href="/AcademiepourTous/">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=egde" />
